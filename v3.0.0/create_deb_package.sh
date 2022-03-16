31d20814d1b03eb22041e1d23fe62f5b2671d8d5
Vitor Henrique A. H. S. Silva <vitorstraggiotti@hotmail.com>
2022-03-16 12:29:49 -0300

    .

--- v3.0.0/create_deb_package.sh
+++ v3.0.0/create_deb_package.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-mkdir cha_3.0.0_amd64
-mkdir ./cha_3.0.0/DEBIAN
-mkdir ./cha_3.0.0_amd64/usr
-mkdir ./cha_3.0.0 amd64/usr/bin
+CONTROLFILE=./cha_3.0.0_amd64/DEBIAN/control
+BINPATH=./cha_3.0.0_amd64/usr/bin
 
-#cp ./cha ./cha_3.0.0_amd64/usr/bin/
+mkdir -p "$(dirname $CONTROLFILE)" && touch "$CONTROLFILE"
+mkdir -p "$BINPATH"
 
-echo "Package: cha" >> ./cha_3.0.0_amd64/DEBIAN/control
-echo "Version: 3.0.0" >> ./cha_3.0.0_amd64/DEBIAN/control
-echo "Architecture: all" >> ./cha_3.0.0_amd64/DEBIAN/control
-echo "Essential: no" >> ./cha_3.0.0_amd64/DEBIAN/control
-echo "Priority: optional" >> ./cha_3.0.0_amd64/DEBIAN/control
-echo "Depends: " >> ./cha_3.0.0_amd64/DEBIAN/control
-echo "Maintainer: Vitor Henrique A. H. S. Silva" >> ./cha_3.0.0_amd64/DEBIAN/control
-echo "Description: Encrypt and decrypt files using chacha algorithm" >> ./cha_3.0.0_amd64/DEBIAN/control
+cp ./cha ./cha_3.0.0_amd64/usr/bin/
+
+echo "Package: cha" >> $CONTROLFILE
+echo "Version: 3.0.0" >> $CONTROLFILE
+echo "Architecture: all" >> $CONTROLFILE
+echo "Essential: no" >> $CONTROLFILE
+echo "Priority: optional" >> $CONTROLFILE
+echo "Depends: " >> $CONTROLFILE
+echo "Maintainer: Vitor Henrique A. H. S. Silva" >> $CONTROLFILE
+echo "Description: Encrypt and decrypt files using chacha algorithm" >> $CONTROLFILE
 
 ## preinst and postinst ca be added
 #echo "" >> ./cha_3.0.0_amd64/DEBIAN/postinst
 #chmod 755 ./cha_3.0.0_amd64/DEBIAN/postinst
 
 ## Generate package
-#dpkg-deb --build cha_3.0.0_amd64
+dpkg-deb --build cha_3.0.0_amd64
+
+## Cleaning generated files
+rm -r cha_3.0.0_amd64
