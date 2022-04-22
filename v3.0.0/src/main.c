9e32dc9b9e6c1dcf3a42825fc6bbf7275e42534e
Vitor Henrique A. H. S. Silva <vitorstraggiotti@hotmail.com>
2022-04-22 18:27:38 -0300

    Delete test code

--- v3.0.0/src/main.c
+++ v3.0.0/src/main.c
@@ -277,13 +277,6 @@ int main(int argc, char *argv[])
     Graph = init_bar_graph('|', '#', ' ', '|');
 
     //Encryption on full size blocks (64 bytes)
-///////
-    for(int i = 0; i < 32; i++)
-    {
-    	Key[i] = 0;
-    }
-    Nonce = 0;
-///////
     BlockCounter = 0;
     for(uint64_t Block = 0; Block < (InFileSizeByte / CIPHER_LENGTH); Block++)
     {
