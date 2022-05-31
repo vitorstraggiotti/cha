c566b275d06c99ad98e04ee8debbaf52ce869c70
Vitor Henrique A. H. S. Silva <vitorstraggiotti@hotmail.com>
2022-05-30 21:08:56 -0300

    .

--- v4.0.0/src/validate_cha.c
+++ v4.0.0/src/validate_cha.c
@@ -5,6 +5,11 @@
 	Start date: 29/05/2022 (DD/MM/YYYY)
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+/* Chacha test vectors:
+https://datatracker.ietf.org/doc/html/draft-strombergson-chacha-test-vectors-00
+   Chacha algorithm:
+https://datatracker.ietf.org/doc/html/rfc7539 */
+
 #include <stdio.h>
 
 #include "validate_cha.h"
